f2c484aae7ce6e5527fdf29ba3c17dedc5d87347
arkar <testhtetarkar@gmail.com>
2025-04-02 13:28:08 +0630

    change host for server

--- backend.py
+++ backend.py
@@ -114,5 +114,6 @@ def editmdy(user_id):
     conn.close()
     return render_template("mdy_food_edit.html", user=user)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
